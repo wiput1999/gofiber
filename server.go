09ecc686589479ee174c76c3dbc548d24343e92f
Wiput Pootong <wiput.pootong@gmail.com>
2020-12-21 22:08:02 +0700

    add parameter route

--- server.go
+++ server.go
@@ -3,12 +3,15 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
 
-    app.Listen(":3000")
+	app.Get("/:value", func(c *fiber.Ctx) error {
+		return c.SendString(c.Params("value"))
+	})
+
+	app.Listen(":3000")
 }
-
